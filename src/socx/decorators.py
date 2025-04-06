c18e495d892b2770fcd4d9c751b5342e2d0958cd
Sagi Kimhi <sagi.kimhi@wiliot.com>
2025-04-06 21:53:41 +0300

    fix(decorators): log_it now logs to global socx.log run log
    Signed-off-by: Sagi Kimhi <sagi.kimhi@wiliot.com>

--- src/socx/decorators.py
+++ src/socx/decorators.py
@@ -1,16 +1,8 @@
-from inspect import getmodule
 from inspect import signature
 from functools import wraps
-from functools import cache
-
-from .log import get_logger
-from ._config import USER_LOG_DIR
 
 
 __all__ = ("log_it",)
-
-
-_loggers = {}
 
 
 def log_it(f):
@@ -22,13 +14,10 @@ def log_it(f):
 
     @wraps(f)
     def wrapper(*args, **kwargs):
-        @cache
-        def logger(f):
-            mod = getmodule(f)
-            return get_logger(mod.__name__, USER_LOG_DIR / mod.__name__)
+        from .log import logger
         sig = f"{f.__name__}{signature(f)}"
-        logger(f).debug(f"{sig}: entered.")
+        logger.debug(f"{sig}: entered.")
         rv = f(*args, **kwargs)
-        logger(f).debug(f"{sig}: returned {rv=}.")
+        logger.debug(f"{sig}: returned {rv=}.")
         return rv
     return wrapper
