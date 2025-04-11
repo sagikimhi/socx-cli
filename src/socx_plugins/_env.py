d15e5f155a7f5e952d67c26c6ff1cfca9db5d075
Sagi Kimhi <sagi.kim5@gmail.com>
2025-04-11 14:25:13 +0300

    refactor(plugins/env): cleanup env code and add debug logging
    Signed-off-by: Sagi Kimhi <sagi.kim5@gmail.com>

--- src/socx_plugins/_env.py
+++ src/socx_plugins/_env.py
@@ -1,18 +1,18 @@
-from functools import partial
-from collections.abc import Iterable
 from pathlib import Path
+from functools import partial
+from dataclasses import dataclass
+from collections.abc import Iterable
 
 import git
 import rich
 import socx
 import rich_click
+from dynaconf.utils.boxing import DynaBox
 
 
 __all__ = (
-    "command",
-    "output_opt",
+    "Manifest",
     "get_repo",
-    "get_manifest",
     "get_ref_type",
     "get_ref_name",
     "get_repo_name",
@@ -20,21 +20,36 @@ __all__ = (
     "get_commit_hash",
     "get_commit_message",
     "find_repositories",
-    "export_manifest",
-    "create_manifest_table",
 )
 
 
-TextType = str | rich.text.Text
+Box: type = rich.box.Box
 
+Repo: type = git.Repo
 
-logger = socx.get_logger(__name__)
+Text: type = str | rich.text.Text
 
+Table: type = rich.table.Table
 
-command: rich_click.Command = partial(rich_click.command, "env")
+Style: type = DynaBox
 
+Header: type = Text
 
-output_opt: rich_click.Option = partial(
+Column: type = DynaBox
+
+Record: type = Iterable[Text]
+
+Option: type = rich_click.Option
+
+Command: type = rich_click.Command
+
+Console: type = rich.console.Console
+
+ConsoleOptions: type = rich.console.ConsoleOptions
+
+RenderResult: type = rich.console.RenderResult
+
+output_opt: Option = partial(
     rich_click.option,
     "--output",
     "-o",
@@ -64,38 +79,20 @@ def get_repo(path: str | Path) -> git.Repo | None:
     return rv
 
 
-def create_manifest_table(
-    headers: Iterable[TextType],
-    box: rich.box.Box = rich.box.ROUNDED,
-    lines: bool = True,
-    expand: bool = True,
-    title: TextType | None = None,
-) -> rich.table.Table:
-    columns = tuple(rich.table.Column(header) for header in headers)
-    return rich.table.Table(
-        *columns,
-        title=title,
-        box=box,
-        expand=expand,
-        show_lines=lines,
-        show_header=True,
-    )
+def is_git_dir(path: str | Path) -> bool:
+    return bool(get_repo(path))
 
 
-def find_repositories(root_dir: Path) -> Iterable[git.Repo]:
-    dirs = [
-        path for path in root_dir.iterdir() if path.is_dir() and get_repo(path)
-    ]
-    dirs.sort()
-    yield from (get_repo(path) for path in dirs)
-
-
-def export_manifest(path: Path, manifest: dict) -> None:
-    logger.info("Exporting env manifest...")
-    with socx.console.capture() as cap:
-        socx.console.print_json(data=manifest, highlight=False, sort_keys=True)
-    path.write_text(cap.get(), encoding="utf-8")
-    logger.info(f"Env manifest written to: '{path}'")
+def find_repositories(root_dir: str | Path) -> Iterable[git.Repo]:
+    if isinstance(root_dir, str):
+        root_dir = Path(root_dir).resolve()
+    if not root_dir.exists() or not root_dir.is_dir():
+        yield from tuple()
+    else:
+        repo_paths = sorted(
+            filter(is_git_dir, root_dir.iterdir()), key=lambda x: Path(x).name
+        )
+        yield from (get_repo(path) for path in repo_paths)
 
 
 def get_repo_name(repo: git.Repo) -> str:
@@ -126,10 +123,103 @@ def get_commit_message(repo: git.Repo) -> str:
     return repo.git.show("-s", "--pretty=%s")
 
 
-def get_manifest(repo: git.Repo) -> dict:
-    ref_name = get_ref_name(repo)
-    ref_type = get_ref_type(repo)
-    repo_name = get_repo_name(repo)
-    author_date = get_author_date(repo)
-    commit_hash = get_commit_hash(repo)
-    commit_message = get_commit_message(repo)
+@dataclass(init=False)
+class Manifest:
+    style: Style
+    repos: Iterable[Repo]
+    columns: Iterable[Column]
+    headers: Iterable[Header]
+    records: Iterable[Record]
+
+    def __init__(self, root_path: str | Path) -> None:
+        if isinstance(root_path, str):
+            root_path = Path(root_path)
+        self.root = root_path
+        self.repos = list(find_repositories(root_path))
+        self.style = socx.settings.git.manifest.style
+        self.columns = socx.settings.git.manifest.columns
+        self.headers = Manifest.get_headers(self.columns, self.style)
+        self.records = Manifest.get_records(self.columns, self.repos)
+
+    @socx.log_it()
+    def __rich_console__(
+        self, console: Console, options: ConsoleOptions
+    ) -> RenderResult:
+        yield from self.as_rich_table().__rich_console__(console, options)
+
+    @socx.log_it()
+    def print(self, console: Console | None = None) -> None:
+        console = console or socx.console
+        console.print(self)
+
+    @socx.log_it()
+    def as_rich_table(
+        self,
+        box: Box = rich.box.ROUNDED,
+        title: Text | None = None,
+        expand: bool = True,
+        show_lines: bool = True,
+        show_header: bool = True,
+        show_footer: bool = False,
+    ) -> Table:
+        title = title or "Manifest"
+        headers = [rich.table.Column(header) for header in self.headers]
+        t: rich.table.Table = rich.table.Table(
+            *headers,
+            box=box,
+            title=title,
+            expand=expand,
+            show_lines=show_lines,
+            show_header=show_header,
+            show_footer=show_footer,
+        )
+        for record in self.records:
+            t.add_row(*record)
+        return t
+
+    @socx.log_it()
+    def as_json(self) -> dict[str, str]:
+        def to_json(repo):
+            return tuple(c.func(repo) for c in self.columns)
+
+        return {get_repo_name(repo): to_json(repo) for repo in self.repos}
+
+    @socx.log_it()
+    def export_json(self, path: str | Path) -> None:
+        if isinstance(path, str):
+            path = Path(path)
+        with socx.console.capture() as cap:
+            socx.console.print_json(
+                data=self.as_json(), indent=4, sort_keys=True
+            )
+        path.write_text(cap.get(), encoding="utf-8")
+        socx.logger.info(f"Manifest written to: '{path}'.")
+
+    @classmethod
+    @socx.log_it()
+    def get_header(cls, column: Column, style: Style) -> Header:
+        return f"[{style.headers}][{column.style}]{column.name}"
+
+    @classmethod
+    @socx.log_it()
+    def get_content(cls, column: Column, repo: Repo) -> Text:
+        return f"[{column.style}]{column.func(repo)}[/]"
+
+    @classmethod
+    @socx.log_it()
+    def get_record(cls, columns: Iterable[Column], repo: Repo) -> Record:
+        return [cls.get_content(c, repo) for c in columns]
+
+    @classmethod
+    @socx.log_it()
+    def get_headers(
+        cls, columns: Iterable[Column], style: Style
+    ) -> Iterable[Header]:
+        return [cls.get_header(c, style) for c in columns]
+
+    @classmethod
+    @socx.log_it()
+    def get_records(
+        cls, columns: Iterable[Column], repos: Iterable[Repo]
+    ) -> Iterable[Record]:
+        return [cls.get_record(columns, repo) for repo in repos]
--- src/socx_plugins/env.py
+++ src/socx_plugins/env.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 import rich_click as click
-from socx import logger
 from socx import console
 from socx import settings
 from socx import global_options
@@ -9,33 +8,15 @@ from socx import global_options
 from socx_plugins import _env
 
 
-@_env.command()
+@click.command("env")
 @_env.output_opt()
 @global_options()
 @click.pass_context
 def cli(ctx: click.Context, output: Path) -> int:
     """Manage different aspects of your working environment & setup."""
-    try:
-        root_dir = Path(settings.git.root_dir)
-    except AttributeError:
-        logger.exception("Failed to get env status")
-        return 1
-
-    style = settings.git.manifest.style
-    columns = settings.git.manifest.columns
-    headers = tuple(f"[{style.headers}][{c.style}]{c.name}" for c in columns)
-    table = _env.create_manifest_table(headers)
-    manifest = {}
-
-    for repo in _env.find_repositories(root_dir):
-        name = _env.get_repo_name(repo)
-        row = tuple(f"[{c.style}]{c.func(repo)}[/]" for c in columns)
-        refname = _env.get_ref_name(repo)
-        reftype = _env.get_ref_type(repo)
-        refhash = _env.get_commit_hash(repo)
-        manifest[name] = {c.name: c.func(repo) for c in columns}
-        console.print(f"{name}: {refhash} ({reftype}: {refname})")
-        table.add_row(*row)
+    root_dir = Path(settings.git.root_dir)
+    table = _env.Manifest(root_dir)
     console.print(table)
     if output:
-        _env.export_manifest(output, manifest)
+        table.export_json(output)
+    ctx.exit(0)
