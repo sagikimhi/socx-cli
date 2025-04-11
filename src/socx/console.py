d65333a6dc39d3c518472db4e60c3934694af8fa
Sagi Kimhi <sagi.kim5@gmail.com>
2025-04-11 14:13:54 +0300

    feat(console): install pretty tracebacks on console
    Signed-off-by: Sagi Kimhi <sagi.kim5@gmail.com>

--- src/socx/console.py
+++ src/socx/console.py
@@ -1,8 +1,18 @@
 from rich import pretty
+from rich import traceback
 from rich.console import Console
 
 __all__ = ("console",)
 
 _console: Console = Console(record=True, tab_size=4, force_terminal=True)
-pretty.install(_console, overflow="ignore", indent_guides=True, max_length=78)
+pretty.install(
+    console=_console, overflow="ignore", indent_guides=True, max_length=78
+)
+traceback.install(
+    theme="nord-darker",
+    console=_console,
+    word_wrap=True,
+    # show_locals=True,
+    indent_guides=True,
+)
 console = _console
