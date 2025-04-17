cdde915b554ab3d7d8684d5cc847e6ec9cd1f717
Sagi Kimhi <sagi.kim5@gmail.com>
2025-04-17 20:45:16 +0300

    chore(plugins/version): add logging and make output prettier
    Signed-off-by: Sagi Kimhi <sagi.kim5@gmail.com>

--- src/socx_plugins/version.py
+++ src/socx_plugins/version.py
@@ -8,16 +8,19 @@ logger = logging.getLogger(__name__)
 
 
 @click.command()
-def version():
+@socx.log_it()
+def version() -> int:
     """Print version information and exit."""
     import subprocess
 
     proj = socx.settings.metadata.__project__
-    cmd = f"/usr/bin/env python3 -m pip show {proj}"
-    proc = subprocess.run(args=cmd.split(), capture_output=True)
-    stdout, stderr = proc.stdout.decode(), proc.stderr.decode()
-    if stderr:
-        logger.exception(stderr)
-    click.echo(stdout)
+    command = f"/usr/bin/env python3 -m pip show {proj}"
+    process = subprocess.run(command.split(), capture_output=True)
 
+    socx.console.print(process.stdout.decode())
 
+    if process.stderr:
+        logger.error(process.stderr.decode())
+        return 1
+
+    return 0
