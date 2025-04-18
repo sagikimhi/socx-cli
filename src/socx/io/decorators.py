5fdecd5e12893d03961c942200f6089854a4592e
Sagi Kimhi <sagi.kim5@gmail.com>
2025-04-18 12:26:14 +0300

    fix(io): fix mypy errors in decorators.py
    Signed-off-by: Sagi Kimhi <sagi.kim5@gmail.com>

--- src/socx/io/decorators.py
+++ src/socx/io/decorators.py
@@ -1,5 +1,7 @@
 import logging
+from typing import Any
 from inspect import signature
+from collections.abc import Callable
 
 from functools import wraps
 
@@ -8,7 +10,9 @@ from .log import Level
 __all__ = ("log_it",)
 
 
-def log_it(level: Level = Level.DEBUG):
+def log_it(
+    level: Level = Level.DEBUG,
+) -> Callable[[None], Callable[[Any], Any]]:
     """Add automatic debug logging to decorated functions/methods."""
 
     def _log_it(f):
