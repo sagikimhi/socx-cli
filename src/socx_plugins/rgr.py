9b610d4203f462c12a059cb4fe0b6f091b2c91a7
Sagi Kimhi <sagi.kimhi@wiliot.com>
2025-05-21 11:06:10 +0300

    feature(rgr): add tui command, change 'rrfh' command name to 'run'
    Signed-off-by: Sagi Kimhi <sagi.kimhi@wiliot.com>

--- src/socx_plugins/rgr.py
+++ src/socx_plugins/rgr.py
@@ -3,31 +3,34 @@ from __future__ import annotations
 import rich_click as click
 from socx import global_options
 
+from socx_tui import SoCX as SoCX
 from socx_plugins._rgr import options
 
 
 @click.group("rgr")
 @global_options()
-def cli(**opts) -> None:
+def cli() -> None:
     """Perform various regression related actions."""
 
 
 @cli.command()
-@options()
 @global_options()
-def rrfh(input, output):  # noqa: A002
-    """Command alias for rerun-failure-history."""
-    import uvloop
-    from socx_plugins._rgr import _run_from_file
-
-    uvloop.run(_run_from_file(input, output))
+def tui() -> None:
+    """Open regression dashboard TUI (Terminal User Interface)."""
+    SoCX().run()
 
 
 @cli.command()
 @options()
 @global_options()
-def rerun_failure_history(input, output):  # noqa: A002
-    """Rerun failed tests from all past regressions."""
+def run(input, output):  # noqa: A002
+    """Run regression from a file of test commands.
+
+    The file can get passed via flag, or, if left unspecified,
+    will be searched according to configuration.
+
+    For more info regarding configurations, check out `socx config` command.
+    """
     import uvloop
     from socx_plugins._rgr import _run_from_file
 
