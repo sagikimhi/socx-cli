666c43e1cf6bccb065344a4bf3c6fe15ed157dc1
Sagi Kimhi <sagi.kimhi@wiliot.com>
2025-07-19 11:14:16 +0300

    feature(socx_plugins.rgr): handle arguments with callbacks
    Signed-off-by: Sagi Kimhi <sagi.kimhi@wiliot.com>

---
+++ src/socx_plugins/rgr/callbacks.py
@@ -0,0 +1,40 @@
+# type: ignore
+
+from __future__ import annotations
+
+from pathlib import Path
+
+from socx import settings, log, log_it
+from rich_click import Context, Parameter
+
+
+logger = log.get_logger(__name__)
+
+
+@log_it(logger=logger)
+def input_cb(ctx: Context, param: Parameter, value: str) -> str:
+    if value:
+        path = Path(value)
+
+        if path.exists() and path.is_file():
+            input_ = {"filename": path.name, "directory": path.parent}
+            settings.set("regression.run.input", input_, merge=True)
+
+    file: str = settings.get("regression.run.input.filename")
+    directory: Path = Path(settings.get("regression.run.input.directory"))
+    input_path = directory / file
+    return input_path
+
+
+@log_it(logger=logger)
+def output_cb(ctx: Context, param: Parameter, value: str) -> str:
+    if value:
+        path = Path(value)
+
+        if path.exists() and path.is_dir():
+            settings.set(
+                "regression.run.output", {"directory": path}, merge=True
+            )
+
+    output_path: Path = Path(settings.get("regression.run.output.directory"))
+    return output_path
