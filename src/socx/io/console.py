d9c3e520addde8307f86f44a4093349d718f61ad
Sagi Kimhi <sagi.kim5@gmail.com>
2025-11-11 17:28:16 +0200

    refactor(io.console): stop using global rich.console.console instance
    Signed-off-by: Sagi Kimhi <sagi.kim5@gmail.com>

--- src/socx/io/console.py
+++ src/socx/io/console.py
@@ -2,21 +2,29 @@
 
 from rich import pretty
 from rich import traceback
-from rich import reconfigure, get_console
 from rich.console import Console
 
-__all__ = ("console",)
+__all__ = ("console", "get_console")
 
-reconfigure(tab_size=4, record=True, markup=True)
-pretty.install(indent_guides=True)
-traceback.install(
-    theme="nord",
-    suppress=["click", "rich_click", "dynaconf"],
-    word_wrap=True,
-    code_width=79,
-    extra_lines=3,
-    show_locals=True,
-    locals_hide_sunder=False,
-    locals_hide_dunder=True,
-)
+
+_DEFAULTS = dict(tab_size=4, markup=True, highlight=True, record=False)
+
+
+def get_console(*args, indent_guides: bool = True, **kwargs) -> Console:
+    kwargs = {**_DEFAULTS, **kwargs}
+    console = Console(*args, **kwargs)
+    pretty.install(console, indent_guides=indent_guides)
+    traceback.install(
+        console=console,
+        suppress=["click", "rich_click", "dynaconf"],
+        word_wrap=True,
+        code_width=79,
+        extra_lines=3,
+        show_locals=True,
+        locals_hide_sunder=True,
+        locals_hide_dunder=True,
+    )
+    return console
+
+
 console: Console = get_console()
