d30aa9dd2b577d7578eda89293e5bb4f41149c78
Sagi Kimhi <sagi.kim5@gmail.com>
2025-12-25 20:00:50 +0200

    chore: flatten socx_tui unnecessary nested packages
    Signed-off-by: Sagi Kimhi <sagi.kim5@gmail.com>

--- src/socx_tui/regression/__init__.py
+++ src/socx_tui/regression/__init__.py
@@ -10,6 +10,8 @@ __all__ = (
 
 
 from socx_tui.regression.app import SoCX as SoCX
+
+from socx_tui.regression.table import Table as Table
+from socx_tui.regression.table import TableVisitor as TableVisitor
+
 from socx_tui.regression.bindings import VimMode as VimMode
-from socx_tui.regression.regression.table import Table as Table
-from socx_tui.regression.regression.table import TableVisitor as TableVisitor
--- src/socx_tui/regression/regression/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Expose regression-related widgets used by the SoCX TUI."""
-
-__all__ = (
-    "Table",
-    "TableVisitor",
-)
-
-from socx_tui.regression.regression.table import Table as Table
-from socx_tui.regression.regression.table import TableVisitor as TableVisitor
