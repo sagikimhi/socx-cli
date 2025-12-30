d38dad5ddb9f1641397388e5edb456cb02ca7f47
Sagi Kimhi <sagi.kim5@gmail.com>
2025-12-30 12:01:35 +0200

    fix(socx.cli): correct exports
    Signed-off-by: Sagi Kimhi <sagi.kim5@gmail.com>

--- src/socx/cli/__init__.py
+++ src/socx/cli/__init__.py
@@ -10,10 +10,12 @@ __all__ = (
     "verbosity",
     "config_files",
     "param_cb",
-    "multi_param_cb",
     "debug_cb",
+    "color_cb",
     "verbosity_cb",
     "configure_cb",
+    "multi_param_cb",
+    "config_files_cb",
     "Decorator",
     "AnyCallable",
 )
@@ -35,8 +37,10 @@ from socx.cli.params import configure as configure
 from socx.cli.params import verbosity as verbosity
 from socx.cli.params import config_files as config_files
 
+from socx.cli.callbacks import debug_cb as debug_cb
 from socx.cli.callbacks import param_cb as param_cb
-from socx.cli.callbacks import multi_param_cb as multi_param_cb
-from socx.cli.callbacks import param_cb as debug_cb
-from socx.cli.callbacks import configure_cb as configure_cb
+from socx.cli.callbacks import color_cb as color_cb
 from socx.cli.callbacks import verbosity_cb as verbosity_cb
+from socx.cli.callbacks import configure_cb as configure_cb
+from socx.cli.callbacks import multi_param_cb as multi_param_cb
+from socx.cli.callbacks import config_files_cb as config_files_cb
