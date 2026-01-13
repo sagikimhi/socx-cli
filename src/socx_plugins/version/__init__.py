b1c82aeccaba1412df99243a10445a7cb4371b4c
Sagi Kimhi <sagi.kim5@gmail.com>
2026-01-13 16:13:14 +0200

    refactor(socx_plugins.version): display colored version output with pip
    Signed-off-by: Sagi Kimhi <sagi.kim5@gmail.com>

--- src/socx_plugins/version/__init__.py
+++ src/socx_plugins/version/__init__.py
@@ -2,12 +2,14 @@
 
 from __future__ import annotations
 
-from plumbum import local, FG
+from plumbum import local
+from socx import APP_ROOT_DIR, console
 
 __all__ = ("version",)
 
 
 def version() -> None:
     """Print version information and exit."""
-    version_cmd = local.python["-m", "uv", "version", "--package", "socx-cli"]
-    _ = version_cmd & FG
+    version_cmd = local.python["-m", "uv", "run", "pip", "show", "socx-cli"]
+    with local.cwd(APP_ROOT_DIR):
+        console.print(version_cmd())
