057a22a6c3d81831add95568a659942c35f6010f
Sagi Kimhi <sagi.kim5@gmail.com>
2026-01-17 16:32:10 +0200

    fix(socx.core.funcs): fix type annotation to support py310 syntax
    Signed-off-by: Sagi Kimhi <sagi.kim5@gmail.com>

--- src/socx/core/funcs.py
+++ src/socx/core/funcs.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
+from typing import TypeVar
 from collections.abc import Iterable
 
 
-def deduplicate[T](collection: Iterable[T]) -> list[T]:
+T = TypeVar("T")
+
+
+def deduplicate(iterable: Iterable[T]) -> list[T]:  # noqa: UP047
     seen = set()
-    return [v for v in collection if v not in seen and seen.add(v) is None]
+    return [v for v in iterable if v not in seen and seen.add(v) is None]
